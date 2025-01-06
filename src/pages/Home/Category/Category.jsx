e54c406ea5fc66065e0a57425589e63b99f7cc4a
unknown <arifinshohidul@gmail.com>
2025-01-05 22:39:40 -0800

    Section title component used

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -8,11 +8,18 @@ import slide1 from '../../../assets/home/slide1.jpg'
 import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 
 const Category = () => {
   return (
-    <Swiper
+    <div>
+        <SectionTitle
+        subHeading = 'Please Order Online'
+        heading = 'Please Order'
+        
+        />
+        <Swiper
     slidesPerView={3}
     spaceBetween={30}
     pagination={{
@@ -43,6 +50,7 @@ const Category = () => {
     </SwiperSlide>
     
   </Swiper>
+    </div>
   )
 }
 
