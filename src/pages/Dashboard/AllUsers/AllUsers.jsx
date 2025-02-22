1834b8a4dcb02d53ed07b521c20ecb05c2d6dd58
unknown <arifinshohidul@gmail.com>
2025-02-22 02:39:45 -0800

    user info saved to database and made user as a admin with admin routes

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,11 +1,13 @@
 import { useQuery } from "@tanstack/react-query"
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaTrash, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
-    // We will use user data only here that's why we don't need to create seperate hook.((
+    // We will use user data only here that's why we don't need to create seperate hook.
     const axiosSecure = useAxiosSecure();
-    const {data: users=[]} = useQuery({
+    const {data: users=[], refetch} = useQuery({
         queryKey: ['users'],
         queryFn: async ()=>{
             const result = await axiosSecure.get('/users')
@@ -15,6 +17,55 @@ const AllUsers = () => {
        
 
     })
+
+    const handleMakeAdmin=(user)=>{
+      axiosSecure.patch(`/users/admin/${user._id}`)
+      .then(result=>{
+        console.log(result.data)
+        if(result.data.modifiedCount >0){
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an admin`,
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
+      })
+    }
+
+    const handleDelete= (user)=>{
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+            axiosSecure.delete(`/users/${user._id}`)
+            .then(res=>{
+                console.log(res)
+                if(res.data.deletedCount >0){
+                    refetch();
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                      });
+
+                }
+            })
+          
+        }
+      });
+
+
+    }
+    
   return (
     <div>
         <div className="flex justify-evenly">
@@ -28,19 +79,21 @@ const AllUsers = () => {
     {/* head */}
     <thead>
       <tr>
-        <th></th>
+        <th>#</th>
         <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Action</th>
       </tr>
     </thead>
     <tbody>
-      {users.map(user=>
+      {users.map((user, index)=>
         <tr key={user._id}>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
+        <th>{index +1}</th>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.role ==="admin"?'Admin':<button onClick={()=> handleMakeAdmin(user)} className="btn btn-ghost btn-xs"><FaUsers className="text-xl"/></button>}</td>
+        <td><button onClick={()=> handleDelete(user)} className="btn btn-ghost btn-xs"><FaTrash className="text-xl"/></button></td>
       </tr>
       )}
       
--- src/steps.js
+++ src/steps.js
@@ -1,4 +1,19 @@
 /**
+ * context api used for Auth related info. Here firebase is used
+ */
+
+
+
+/**
+ * Added to cart using axios.post method
+ * cart data showed to Cart.jsx, using useCart hook where tanstact query added data extracted
+ * If we use tanstact query we dont need to use useEffect and useState.
+ * we can use refetch in tanstack query
+ */
+
+
+
+/**
  * How to make dashboard:
  * create dashboard layout
  * create dashboard routes
@@ -8,4 +23,42 @@
  * How to make admin dashboard and users dashboard seperately
  * const isAdmin = true;
  * {isAdmin? <></>: <></>}
+ * From the list of users in the db we can make admin in the following way:
+ * Backend code:
+ * app.patch('/users/admin/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+      $set: {
+        role: 'admin'
+      },
+    };
+    const result = await userCollection.updateOne(filter, updateDoc);
+    res.send(result)
+    })
+    
+    FrontEnd code:
+    const handleMakeAdmin=(user)=>{
+          axiosSecure.patch(`/users/admin/${user._id}`)
+          .then(result=>{
+            console.log(result.data)
+            if(result.data.modifiedCount >0){
+              refetch();
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: `${user.name} is an admin`,
+                showConfirmButton: false,
+                timer: 1500
+              });
+            }
+          })
+        }
+    <td>{user.role ==="admin"?'Admin':<button onClick={()=> handleMakeAdmin(user)} className="btn btn-ghost btn-xs"><FaUsers className="text-xl"/></button>}</td>
  */
+
+    /**
+     * Json web token implementation:
+     * npm i jsonwebtoken
+     * const jwt = require('jsonwebtoken');
+     */
