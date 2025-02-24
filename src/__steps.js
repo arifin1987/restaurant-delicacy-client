12533aa73a005222255304dc06032768b3c84d42
unknown <arifinshohidul@gmail.com>
2025-02-23 21:50:00 -0800

    for 401 or 403 logout the user and move the user to the login page

--- src/__steps.js
+++ src/__steps.js
@@ -87,6 +87,7 @@
         while api calling.
 
 import axios from "axios";
+
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
@@ -99,9 +100,24 @@ const useAxiosSecure = () => {
     return config;
   }, function(error){
     return Promise.reject(error);
+  });
+  
+  // intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(function(response){
+    return response;
+  },async function(error){
+    const status = error.response.status;
+    console.log('status error in the interceptor', status);
+    // for 401 or 403 logout the user and move the user to the login page
+    if(status ===401 || status === 403){
+      await logOut();
+      navigate('/login');
+    }
+    return Promise.reject(error);
   })
-  return axiosSecure;
+      return axiosSecure;
 }
+
 
 export default useAxiosSecure
 
